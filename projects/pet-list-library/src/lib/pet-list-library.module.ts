107a3a49ea2935e0e1daea66aaaa711dadad2a0b
Carlos <cgarciat@viewnext.com>
2020-12-02 11:10:56 +0100

    exportados modulos

--- projects/pet-list-library/src/lib/pet-list-library.module.ts
+++ projects/pet-list-library/src/lib/pet-list-library.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PetListLibraryComponent } from './pet-list-library.component';
-// import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-
 @NgModule({
   declarations: [PetListLibraryComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    // HttpClientModule
-  ],
-  exports: [PetListLibraryComponent]
+  imports: [CommonModule, FormsModule],
+  exports: [PetListLibraryComponent, FormsModule, CommonModule],
 })
-export class PetListLibraryModule { }
+export class PetListLibraryModule {}
